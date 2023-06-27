787120e66faa2fee9735c0e938da075bb7b075d2
suportedoterp <jrdiasx@gmail.com>
2023-06-27 10:46:06 -0300

    quase lá

--- Wasting time/testes/principal.py
+++ Wasting time/testes/principal.py
@@ -1,10 +1,26 @@
-antigo = open('antigo.txt', 'r')
+antigo = open('Wasting time/testes/antigo.txt', 'r+')
 listaAntigo = []
 
 for linha in antigo:
-    print(linha)
-    # listaAntigo.append(linha)
-
+    linha = linha.strip()
+    listaAntigo.append(linha)
 antigo.close()  
 
-print(listaAntigo)
+
+novo = open('Wasting time/testes/novo.txt', 'r+')
+listaNovo = []
+
+for linha in novo:
+    linha = linha.strip()
+    listaNovo.append(linha)
+novo.close()  
+
+
+terceira = []
+b = 0
+for i in listaNovo:
+    if listaNovo[b] in listaAntigo[b]:
+        terceira.append(listaNovo[b])
+        b += 1
+
+print(terceira)
