f8ba65934ef0238410ba412ae9826b370cd44613
suportedoterp <jrdiasx@gmail.com>
2023-06-30 17:46:40 -0300

    aprendendo api

--- Wasting time/Python/Consumindo API/main.py
+++ Wasting time/Python/Consumindo API/main.py
@@ -1,16 +1,31 @@
 import requests
 import json
 
-response = requests.get('https://economia.awesomeapi.com.br/last/GBP-BRL')
 
-if response.status_code == 200:
-    data = response.json()
-else:
-    print('Erro na requisição. Código de status: {response.status_code}')
+def request(URL):
+    response = requests.get(URL)
+    if response.status_code == 200:
+        data = json.loads(response.content)
+        data = data['GBPBRL.code']
+        return data
+    else:
+        return 'Erro na requisição. Código de status: {response.status_code}'
 
-data = str(data)
+response = request('https://economia.awesomeapi.com.br/last/GBP-BRL')
 
-json_data = json.loads(data)
 
-print(json_data["high"])
+print(response) 
 
+
+# response = requests.get('https://economia.awesomeapi.com.br/last/GBP-BRL')
+
+# if response.status_code == 200:
+#     data = response.json()
+# else:
+#     print('Erro na requisição. Código de status: {response.status_code}')
+
+# data = str(data)
+
+# json_data = json.loads(data)
+
+# print(json_data["high"])
