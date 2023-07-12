cf2cbffcc4badc85f7cde917ee571e75516a0616
suportedoterp <jrdiasx@gmail.com>
2023-07-12 17:56:06 -0300

    Começo orientação a objeto

--- Wasting time/Python/Consumindo API/main.py
+++ Wasting time/Python/Consumindo API/main.py
@@ -1,6 +1,17 @@
 import requests
 import json
 
+class ConverterMoedas():
+    def __init__(self):
+        moeda = 
+
+
+
+
+
+conversao = input(str('Qual conversão deseja? '))
+moeda_desejada = conversao.index('-')
+moeda_desejada = conversao[0:moeda_desejada]
 
 def request(URL, moeda):
     response = requests.get(URL)
@@ -14,7 +25,17 @@ def request(URL, moeda):
     except:
         print('Erro na requisição, verifique sua request!')
 
-response = request('https://economia.awesomeapi.com.br/last/GBP-BRL', 'GBPBRL')
+response = request(f'https://economia.awesomeapi.com.br/last/{conversao}', f'{moeda_desejada}')
+print(response)
 
-
-print(response) 
+# def request(URL, moeda):
+#     response = requests.get(URL)
+#     try:
+#         if response.status_code == 200:
+#             data = json.loads(response.content)
+#             data = data[f'{moeda}']
+#             return data['bid']
+#         else:
+#             return f'Erro na requisição. Código de status: {response.status_code}'
+#     except:
+#         print('Erro na requisição, verifique sua request!')
